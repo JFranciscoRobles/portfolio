2c1aa1b405fa47d7c195e2b7e880d2e93ca8fdab
JFranciscoRobles <josefrancisco.roblesacosta@gmail.com>
2022-05-20 17:19:05 -0700

    pwa

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -1,12 +1,21 @@
 const withTM = require("next-transpile-modules")(["ui"]);
+const withPWA = require("next-pwa");
 
-module.exports = withTM({
-  reactStrictMode: true,
-  i18n: {
-    defaultLocale: "es",
-    locales: ["en", "es"],
-  },
-  images: {
-    format: ["image/webp", "image/png", "image/jpeg"],
-  },
-});
+module.exports = withPWA(
+  withTM({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === "development",
+    },
+    reactStrictMode: true,
+    i18n: {
+      defaultLocale: "es",
+      locales: ["en", "es"],
+    },
+    images: {
+      format: ["image/webp", "image/png", "image/jpeg"],
+    },
+  })
+);
